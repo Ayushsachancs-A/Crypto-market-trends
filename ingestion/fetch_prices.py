e58f6457a9319706e5e8b576f3ad0c0b76cd3034
Ayush Sachan <ayushsachan162@gmail.com>
2025-06-03 17:44:15 +0530

    fetch

---
+++ ingestion/fetch_prices.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+from datetime import datetime, UTC
+import boto3
+from io import StringIO
+import os
+
+def fetch_prices():
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd"
+    response = requests.get(url)
+    data = response.json()
+
+    df = pd.DataFrame(data).T
+    df.columns = ['price_usd']
+    df['timestamp'] = datetime.now(UTC)
+    df.index.name = 'coin'
+    return df.reset_index()
+
+def upload_to_s3(df, bucket, filename):
+    # Convert DataFrame to CSV string
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    # Upload using boto3
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket=bucket, Key=filename, Body=csv_buffer.getvalue())
+
+if __name__ == "__main__":
+    df = fetch_prices()
+    filename = f"raw/crypto_{datetime.now(UTC).strftime('%Y%m%d_%H%M%S')}.csv"
+    upload_to_s3(df, 'crypto-transform-bucket', filename)
+    print("✅ Uploaded to S3:", filename)

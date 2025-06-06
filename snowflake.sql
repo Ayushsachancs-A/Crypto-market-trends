6100fa20803f520cbab2054c279b11589a8538fe
Ayush Sachan <ayushsachan162@gmail.com>
2025-06-06 11:16:36 +0530

    Create snowflake.sql

---
+++ snowflake.sql
@@ -0,0 +1,45 @@
+-- Create database and schema
+CREATE OR REPLACE DATABASE crypto_datalake;
+USE DATABASE crypto_datalake;
+CREATE OR REPLACE SCHEMA raw_data;
+
+-- Create target table for crypto prices
+CREATE OR REPLACE TABLE raw_data.crypto_prices (
+    coin STRING,
+    price_usd FLOAT,
+    timestamp TIMESTAMP
+);
+
+-- Create external stage to point to your S3 bucket
+CREATE OR REPLACE STAGE s3_stage
+URL='s3://crypto-transform-bucket/raw/'
+STORAGE_INTEGRATION = (skip this for now)
+CREDENTIALS = (AWS_KEY_ID='<your_access_key>' AWS_SECRET_KEY='<your_secret_key>');
+
+-- View files in the S3 stage
+LIST @s3_stage;
+
+-- Copy CSV from S3 to Snowflake table
+COPY INTO raw_data.crypto_prices
+FROM @s3_stage
+FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1);
+
+-- Show the latest 5 entries
+SELECT * FROM raw_data.crypto_prices
+ORDER BY timestamp DESC
+LIMIT 5;
+
+-- Average price by coin
+SELECT coin, AVG(price_usd) AS avg_price
+FROM raw_data.crypto_prices
+GROUP BY coin;
+
+--Trend Matrics
+SELECT
+    coin,
+    timestamp,
+    price_usd,
+    AVG(price_usd) OVER (PARTITION BY coin ORDER BY timestamp ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS moving_avg,
+    LAG(price_usd, 24) OVER (PARTITION BY coin ORDER BY timestamp) AS price_24h_ago
+FROM crypto_prices
+ORDER BY timestamp DESC;
